ab20eec415d1752b0e9a320346ac2f1bbe674b22
Lopez Pacheco <rylopez@minsait.com>
2025-05-07 15:25:46 -0600

    ⚙️ Fix routing: rutas Login y Registro configuradas correctamente

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Login from './pages/Login';
+import Registro from './pages/Registro';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registro" element={<Registro />} />
+        </Routes>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
